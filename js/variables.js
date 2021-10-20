6594ee84b4add6d61471801165084681dcc3c4a6
Bryan Capdevila <capdevilabg@gmail.com>
2021-10-20 11:10:18 -0300

    primer subida al repo

---
+++ js/variables.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Cual es su nombre?")
+let edad = parseInt(prompt("Cual es su edad ?"))
+
+
+alert("Hola " + nombre)
+alert("Hace" + " " + (edad - 18) + " " + "años que eres mayor de edad !")
