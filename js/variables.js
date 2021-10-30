fd5adb0bfd1048f462c11c98f250ae7c77dbaf60
Bryan Capdevila <capdevilabg@gmail.com>
2021-10-30 10:04:27 -0300

    funciones

--- js/variables.js
+++ js/variables.js
@@ -20,7 +20,7 @@ if (adminValido) {
 
 /* ------------------------------------------------------------------------------------ */
 
-
+/* 
 
 
 let color = "";
@@ -35,3 +35,34 @@ for (let i = 1; i < 6; i++) {
     document.write("<h" + i + ">" + "Titulo" + i + "</h" + i + ">");
 
 }
+ */
+
+
+
+
+
+
+
+let cantidadParticipantes = parseInt(prompt("Cuantos participantes van a jugar?"))
+
+
+function saludar(nombre) {
+    alert("Hola " + nombre);
+}
+
+function pedirNombre(cantidad, mensaje) {
+    for (let i = 1; i < cantidad; i++) {
+        let nombre = prompt(mensaje);
+        saludar(nombre + " usted es el participante numero " + i);
+    }
+}
+
+function random(min, max) {
+    alert("Quien comienza el juego es el participante numero : " + (Math.floor((Math.random() * (max - min + 1)) + min)));
+}
+
+
+
+
+pedirNombre((cantidadParticipantes + 1), "Cual es su nombre?");
+random(1, cantidadParticipantes);
