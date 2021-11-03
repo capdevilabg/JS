46aaab653a8de3a2c8bd23c52f020fb9c432db3c
Bryan Capdevila <capdevilabg@gmail.com>
2021-11-03 11:13:00 -0300

    objetos

--- js/variables.js
+++ js/variables.js
@@ -1,27 +1,35 @@
 
 
-let cantidadParticipantes = parseInt(prompt("Cuantos participantes van a jugar?"))
+function Jugador(nombre, nivel) {
+    this.nombre = nombre;
+    this.nivel = nivel;
 
-
-function saludar(nombre) {
-    alert("Hola " + nombre);
 }
 
-function pedirNombre(cantidad, mensaje) {
-    for (let i = 1; i < cantidad; i++) {
-        let nombre = prompt(mensaje);
-        saludar(nombre + " usted es el participante numero " + i);
+function PedirDatos(cantidad) {
+    let niveles = 0 //declaro niveles como un acumulador para despues sacar el promedio 
+    let jugadores = {};//esto es un objeto con objetos adentro cuando corra el for 
+
+    for (let i = 0; i < cantidad; i++) {
+        let nombre = prompt("Cual es su nombre?");//pide nnombre
+        let nivel = parseInt(prompt("Cual es su nivel ?"));//pide nivel 
+
+        jugadores[i] = new Jugador(nombre, nivel);//personas en la posicion [i] (al principio es posicion 0 y va incrementando con el for)
+        niveles += jugadores[i].nivel; //aca suma el nivel del objeto creado a "niveles"
+
     }
+
+
+   alert("El promedio de nivel es de " + niveles / cantidad); // aca muestro la lista de personas , y el promedio de nivel 
 }
 
-function random(min, max) {
-    alert("Quien comienza el juego es el participante numero : " + (Math.floor((Math.random() * (max - min + 1)) + min)));
-}
+PedirDatos(3);// aca llamo a pedirdatos 3 veces 
 
 
 
 
-pedirNombre((cantidadParticipantes + 1), "Cual es su nombre?");
-random(1, cantidadParticipantes);
+
+
+
 
 
